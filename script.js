663a0f585be269ae2df2cc8b00dc56ae410b9b94
Vinicius Alves <vinalves2003@gmail.com>
2023-01-07 17:03:31 -0300

    functions

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function inserting(number){
-    const result = document.getElementById("result")
-    return result.innerHTML = number.toString()
+    const result = document.getElementById("result").innerHTML;
+    document.getElementById("result").innerHTML = result + number;
+    
+}
+
+function clean(){
+    document.getElementById("result").innerHTML = "0";
 }
